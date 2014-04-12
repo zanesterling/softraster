622e81770788d47faf9dce080c887a5dafdb9461
Zane Sterling <sterling.zane@gmail.com>
2014-04-11 21:26:47 -0400

    Add window infrastructure

---
+++ headers/window.h
@@ -0,0 +1,8 @@
+#ifndef WINDOW
+#define WINDOW
+
+void drawToScreen();
+bool init(int width, int height);
+void clean_up();
+
+#endif
---
+++ window.cpp
@@ -0,0 +1,63 @@
+#include "headers/window.h"
+
+const int SCREEN_WIDTH  = 250;
+const int SCREEN_HEIGHT = 250;
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+SDL_Texture *drawTexture = NULL;
+SDL_Surface *drawSurface = NULL;
+
+void drawToScreen() {
+    SDL_UpdateTexture(drawTexture, NULL, drawSurface->pixels, drawSurface->pitch);
+    SDL_RenderCopy(renderer, drawTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+}
+
+// set up SDL and related components
+bool init(int width, int height) {
+    // if the init fails, display an error message and exit
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        error("SDL could not initialize");
+        return false;
+    }
+
+    // if the window fails to load, display an error message and exit
+    window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, width, height,
+                              SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+		error("Window could not be created");
+		return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                  SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL) {
+		error("Renderer could not be initialized");
+		return false;
+    }
+
+    drawSurface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+    if (drawSurface == NULL) {
+		error("Draw surface could not be created");
+		return false;
+    }
+
+    drawTexture = SDL_CreateTextureFromSurface(renderer, drawSurface);
+    if (drawTexture == NULL) {
+		error("Draw texture could not be created");
+		return false;
+    }
+
+    SDL_RenderClear(renderer);
+    return true;
+}
+
+void clean_up() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyTexture(drawTexture);
+	SDL_FreeSurface(drawSurface);
+	SDL_Quit();
+}
